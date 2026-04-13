145493321387a2532d2916fa24cdc118e6f90976
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2026-04-13 10:53:11 +0100

    MISUV-10881 Adding test user for recent activity

---
+++ app/uk/gov/hmrc/incometaxvcfsandstub/utils/ObligationsDataUtils.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxvcfsandstub.utils
+
+import org.mongodb.scala.Document
+
+import java.time.LocalDate
+
+object ObligationsDataUtils {
+  final val obligationsDataKey = "response.obligations"
+  private val today = LocalDate.now()
+
+  def createObligationsData() = {
+    val currentTaxYearStart = if(today.isBefore(LocalDate.of(today.getYear, 4, 6))) LocalDate.of(today.minusYears(1).getYear, 4, 6) else LocalDate.of(today.getYear, 4, 6)
+    val taxYearTaxReturnDueDate = LocalDate.of(currentTaxYearStart.getYear + 1, 1, 31)
+
+    Seq(Document(
+      "identification" -> Document(
+        "incomeSourceType" -> "ITSB",
+        "referenceNumber" -> s"XAIS00000000001",
+        "referenceType" -> "MTDBIS"
+      ),
+      "obligationDetails" -> Seq(
+        Document(
+          "status" -> "F",
+          "inboundCorrespondenceFromDate" -> today.minusMonths(11).toString,
+          "inboundCorrespondenceToDate" -> today.minusMonths(8).toString,
+          "inboundCorrespondenceDueDate" -> today.minusMonths(1).toString,
+          "inboundCorrespondenceDateReceived" -> today.minusMonths(2).toString,
+          "periodKey" -> "#001"
+        ),
+        Document(
+          "status" -> "F",
+          "inboundCorrespondenceFromDate" -> today.minusMonths(8).toString,
+          "inboundCorrespondenceToDate" -> today.minusMonths(5).toString,
+          "inboundCorrespondenceDueDate" -> today.plusMonths(1).toString,
+          "inboundCorrespondenceDateReceived" -> today.minusMonths(1).toString,
+          "periodKey" -> "#002",
+        ),
+    )
+    ),
+      Document(
+        "identification" -> Document(
+          "incomeSourceType" -> "ITSA",
+          "referenceNumber" -> s"XAIT00000000002",
+          "referenceType" -> "MTDBIS"
+        ),
+        "obligationDetails" -> Seq(
+          Document(
+            "status" -> "F",
+            s"inboundCorrespondenceFromDate" -> currentTaxYearStart.minusYears(1).toString,
+            s"inboundCorrespondenceToDate" -> currentTaxYearStart.minusDays(1).toString,
+            "inboundCorrespondenceDueDate" -> taxYearTaxReturnDueDate.toString,
+            "inboundCorrespondenceDateReceived" -> today.toString,
+            "periodKey" -> "C"
+          )
+        )
+      )
+    )
+  }
+}
