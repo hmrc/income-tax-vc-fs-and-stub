3bddfa66c4b0e1866c806d79a697181b2b4b5359
taradhesi14 <233078744+taradhesi14@users.noreply.github.com>
2026-08-07 15:18:36 +0100

    Removal of references to 1896 in CrystallisationStatus

--- app/uk/gov/hmrc/incometaxvcfsandstub/models/CrystallisationStatus.scala
+++ app/uk/gov/hmrc/incometaxvcfsandstub/models/CrystallisationStatus.scala
@@ -35,22 +35,13 @@ case class CrystallisationStatus(
       case _                 => throw UnexpectedException(Some("Status can be only Crystallised or Non-Crystallised"))
     }
 
- // private def is1896: Boolean = taxYear.endYear >= 2024
-
- // private def taxYearField: Option[String] = if (is1896) None else Some(taxYear.rangeLong)
-  
-  //private def calculationTypeField: String = if (is1896) "crystallisation" else "finalDeclaration"
-
   def makeOverwriteJson: JsValue = {
     Json.arr(
       Json.obj(
         "calculationId"        -> "1d35cfe4-cd23-22b2-b074-fae6052024a8",
         "calculationTimestamp" -> s"2024-09-30T09:15:34.0Z",
-      //  "calculationType"      -> calculationTypeField   to change to "finalDeclaration?"
+        "calculationType"      -> "finalDeclaration"
       ) ++
-     // Json.obj(
-     //   "taxYear" -> taxYearField
-     // ) ++
       Json.obj(
         "totalIncomeTaxAndNicsDue" -> 15450,
         "crystallised"             -> isCrystallised,
@@ -59,19 +50,11 @@ case class CrystallisationStatus(
       )
     )
   }
-
- // private def getSchemaIdValue: String = {
-  //  if (is1896) {
-      "getCalculationListDetailsSuccess"
-  //  } else {
-   //   "getListCalculationDetailsSuccess"
-  //  }
- // }
-
+  
   def makeOverwriteDataModel: DataModel =
     DataModel(
       _id = url,
-     // schemaId = getSchemaIdValue, to chnage to schema id name "getCalculationListDetailsSuccessHip"
+      schemaId = "getCalculationListDetailsSuccessHip",
       method = "GET",
       status = expectedStatusCode,
       response = Some(makeOverwriteJson)
