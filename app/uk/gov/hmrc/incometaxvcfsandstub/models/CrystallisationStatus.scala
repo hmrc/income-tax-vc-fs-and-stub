c91f09f666c6c5b9ffe5e1730746d5ebbf794484
taradhesi14 <233078744+taradhesi14@users.noreply.github.com>
2026-08-07 15:13:42 +0100

    MISUV-118636: clean up of stub routes to remove referene to 1896

--- app/uk/gov/hmrc/incometaxvcfsandstub/models/CrystallisationStatus.scala
+++ app/uk/gov/hmrc/incometaxvcfsandstub/models/CrystallisationStatus.scala
@@ -35,22 +35,22 @@ case class CrystallisationStatus(
       case _                 => throw UnexpectedException(Some("Status can be only Crystallised or Non-Crystallised"))
     }
 
-  private def is1896: Boolean = taxYear.endYear >= 2024
+ // private def is1896: Boolean = taxYear.endYear >= 2024
 
-  private def taxYearField: Option[String] = if (is1896) None else Some(taxYear.rangeLong)
-
-  private def calculationTypeField: String = if (is1896) "crystallisation" else "finalDeclaration"
+ // private def taxYearField: Option[String] = if (is1896) None else Some(taxYear.rangeLong)
+  
+  //private def calculationTypeField: String = if (is1896) "crystallisation" else "finalDeclaration"
 
   def makeOverwriteJson: JsValue = {
     Json.arr(
       Json.obj(
         "calculationId"        -> "1d35cfe4-cd23-22b2-b074-fae6052024a8",
         "calculationTimestamp" -> s"2024-09-30T09:15:34.0Z",
-        "calculationType"      -> calculationTypeField
+      //  "calculationType"      -> calculationTypeField   to change to "finalDeclaration?"
       ) ++
-      Json.obj(
-        "taxYear" -> taxYearField
-      ) ++
+     // Json.obj(
+     //   "taxYear" -> taxYearField
+     // ) ++
       Json.obj(
         "totalIncomeTaxAndNicsDue" -> 15450,
         "crystallised"             -> isCrystallised,
@@ -60,18 +60,18 @@ case class CrystallisationStatus(
     )
   }
 
-  private def getSchemaIdValue: String = {
-    if (is1896) {
+ // private def getSchemaIdValue: String = {
+  //  if (is1896) {
       "getCalculationListDetailsSuccess"
-    } else {
-      "getListCalculationDetailsSuccess"
-    }
-  }
+  //  } else {
+   //   "getListCalculationDetailsSuccess"
+  //  }
+ // }
 
   def makeOverwriteDataModel: DataModel =
     DataModel(
       _id = url,
-      schemaId = getSchemaIdValue,
+     // schemaId = getSchemaIdValue, to chnage to schema id name "getCalculationListDetailsSuccessHip"
       method = "GET",
       status = expectedStatusCode,
       response = Some(makeOverwriteJson)
