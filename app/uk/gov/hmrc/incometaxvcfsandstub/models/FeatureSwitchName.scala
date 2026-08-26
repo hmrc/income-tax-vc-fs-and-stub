3c417a903ea495f8c364de81fbbe2d8af61fcee8
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2026-08-26 11:02:29 +0100

    MIPR-2967 updated newHubContextRoot FS

--- app/uk/gov/hmrc/incometaxvcfsandstub/models/FeatureSwitchName.scala
+++ app/uk/gov/hmrc/incometaxvcfsandstub/models/FeatureSwitchName.scala
@@ -269,7 +269,7 @@ case object RevenueAmendments extends FeatureSwitchName {
 }
 
 case object NewHubContextRootEnabled extends FeatureSwitchName {
-  override val name: String = "enable-new-hub-context-root"
+  override val name: String = "new-hub-context-root"
   override val toString: String = "New Hub Context-root Enabled"
 }
 
